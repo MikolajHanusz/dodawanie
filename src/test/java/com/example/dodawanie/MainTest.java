51dce60ddd678692b9923313306b95a4ce1a12bc
DorotaKiczor <dorokic980@student.polsl.pl>
2024-05-13 11:49:04 +0200

    Initial commit

---
+++ src/test/java/com/example/dodawanie/MainTest.java
@@ -0,0 +1,11 @@
+package com.example.dodawanie;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+    public void testAdded() {
+        Main main = new Main();
+        assertEquals(10,main.added(4,6));
+    }
+}
